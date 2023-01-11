29b57e09f8f1622fc9b29654610d5cd7537c7263
Christian Eriksson <kryckan@gmail.com>
2023-01-12 00:18:39 +0100

    bugfix

--- Days/Day 4/rock_paper_scissors.py
+++ Days/Day 4/rock_paper_scissors.py
@@ -35,20 +35,24 @@ pc_rps = random.choice(RPS)
 
 print("Welcome to a game of Rock, Paper, Scissors! \n")
 choice = (
-    int(input("What do you chose? 1 for Rock, 2 for Paper, and 3 for Scissors ")) - 1
+    int(input("What do you chose? 1 for Rock, 2 for Paper, and 3 for Scissors \n")) - 1
 )
-print(RPS[choice])
-print("Rock!")
-time.sleep(0.5)
-print("Paper!")
-time.sleep(0.5)
-print("Scissors!")
-time.sleep(0.5)
-print("Lizzard!")
-time.sleep(0.5)
-print("Spock!")
-time.sleep(0.5)
-print(pc_rps)
+
+if choice > 2:
+    print("\nYou didn't play by the rules. You lose!")
+else:
+    print(RPS[choice])
+    print("Rock!")
+    time.sleep(0.5)
+    print("Paper!")
+    time.sleep(0.5)
+    print("Scissors!")
+    time.sleep(0.5)
+    print("Lizzard!")
+    time.sleep(0.5)
+    print("Spock!")
+    time.sleep(0.5)
+    print(pc_rps)
 
 if choice == 0:
     if pc_rps == ROCK:
@@ -58,7 +62,7 @@ if choice == 0:
     else:
         print("You win!!!")
 
-if choice == 1:
+elif choice == 1:
     if pc_rps == ROCK:
         print("You Win!!!")
     elif pc_rps == PAPER:
@@ -66,7 +70,7 @@ if choice == 1:
     else:
         print("You lose!")
 
-if choice == 2:
+elif choice == 2:
     if pc_rps == ROCK:
         print("You lose!")
     elif pc_rps == PAPER:
