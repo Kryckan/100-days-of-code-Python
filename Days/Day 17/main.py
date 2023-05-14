846e19c82effa8e8f719c4fcf0c0c181f6ca09d0
Christian Eriksson <kryckan@gmail.com>
2023-05-14 11:10:00 +0200

    ad background

---
+++ Days/Day 17/main.py
@@ -0,0 +1,21 @@
+class user:
+
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
+        self.following = 0
+
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
+
+user_1 = user("1", "Chris")
+user_2 = user("2", "Mason")
+
+user_1.follow(user_2)
+
+print(user_1.following)
+print(user_1.followers)
+print(user_2.following)
+print(user_2.followers)
