758d75812e1fb04e0090e4b1e0615de56a930074
Christian Eriksson <kryckan@gmail.com>
2023-06-28 01:30:27 +0200

    add Day 17

--- Days/Day 17/quiz-game-start/main.py
+++ Days/Day 17/quiz-game-start/main.py
@@ -1,14 +1,19 @@
-from question_model import Question
 from data import question_data
+from question_model import Question
+from quiz_brain import QuizBrain
 
-questionBank = []
+question_bank = []
 
 for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question["text"]
+    question_answer = question["answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
 
-    data = Question(question_text, question_answer)
+quiz = QuizBrain(question_bank)
 
-    questionBank.append(data)
+while quiz.still_has_questions():
+    quiz.next_question()
 
-print(questionBank)
+print('You have completed the quiz!')
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
