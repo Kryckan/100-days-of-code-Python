c5cbec360ff5d4ee648b3d87d5e93091d4d6f4d6
Christian Eriksson <kryckan@gmail.com>
2024-03-15 21:18:34 +0100

    score-update-read-highscore-fix (#8)
    * ✨ feat: Add functionality to read and update high score from external file to Score class.
    
    * ✨ Refactor Score class to use type hints for score_count, high_score, and speed_count.
    
    * ✨ Add starting code for day 24 challange

--- Days/0024_Snake_cont/score.py
+++ Days/0024_Snake_cont/score.py
@@ -1,3 +1,4 @@
+import os
 from turtle import Screen, Turtle
 
 screen = Screen()
@@ -6,11 +7,11 @@ FONT = ("Courier", 14, "normal")
 
 
 class Score(Turtle):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
-        self.score_count = 0
-        self.high_score = self.read_highscore()
-        self.speed_count = 0
+        self.score_count: int = 0
+        self.high_score: int = self.read_highscore()
+        self.speed_count: int = 0
         self.level_speed = 0.2
         self.level = 1
         self.ht()
@@ -18,12 +19,12 @@ class Score(Turtle):
         self.goto(0, 280)
         self.color("white")
         self.write(
-            f"Score: {self.score_count} Level: {self.level}, High Score: {self.read_highscore}",
+            f"Score: {self.score_count} Level: {self.level}, High Score: {self.read_highscore()}",
             align=ALIGNMENT,
             font=FONT,
         )
 
-    def increase_score(self):
+    def increase_score(self) -> None:
         self.clear()
         self.score_count += 1
         self.speed_count += 1
@@ -34,39 +35,47 @@ class Score(Turtle):
         )
         self.speed_increase()
 
-    def speed_increase(self):
+    def speed_increase(self) -> None:
         if self.speed_count == 4:
             self.level += 1
             self.level_speed -= 0.01
             self.speed_count = 0
 
-    def reset(self):
+    def reset(self) -> None:
         self.clear()
         if self.score_count > self.high_score:
             self.update_highscore()
-        self.write(
-            f"Score: {self.score_count} Level: {self.level}, High Score: {self.high_score}",
-            align=ALIGNMENT,
-            font=FONT,
-        )
 
         self.score_count = 0
         self.level = 1
         self.level_speed = 0.2
         self.speed_count = 0
+        self.write(
+            f"Score: {self.score_count} Level: {self.level}, High Score: {self.read_highscore()}",
+            align=ALIGNMENT,
+            font=FONT,
+        )
 
-    def read_highscore(self):
+    def read_highscore(self) -> int:
+        # Path relative to the current script
+        directory = os.path.dirname(__file__)
+        file_path = os.path.join(directory, "data.txt")
+
         try:
-            with open("data.txt", mode="r") as file:
+            with open(file_path, mode="r") as file:
                 contents = file.read()
                 return int(contents)
         except FileNotFoundError:
-            with open("data.txt", mode="w") as file:
+            with open(file_path, mode="w") as file:
                 file.write("0")
             return 0
 
-    def update_highscore(self):
+    def update_highscore(self) -> None:
+        # Path relative to the current script
+        directory = os.path.dirname(__file__)
+        file_path = os.path.join(directory, "data.txt")
+
         if self.score_count > self.high_score:
             new_high_score = self.score_count
-            with open("data.txt", mode="w") as file:
+            with open(file_path, mode="w") as file:
                 file.write(str(new_high_score))
