96e3029ef4e911ef13cfd4ea6375316ec5e2495f
Christian Eriksson <kryckan@gmail.com>
2024-03-22 10:46:32 +0100

    ✨ Clean up

--- Days/025-csv/testing_pandas.py
+++ Days/025-csv/testing_pandas.py
@@ -1,9 +1,14 @@
 import os
 
 import pandas as pd
+import polars as pl
 
 root_directory = os.path.dirname(__file__)
 file_path = os.path.join(root_directory, "weather_data.csv")
 
 data = pd.read_csv(file_path)
-print(data)
+
+data2 = pl.read_csv(file_path)
+
+average_temp = data2["temp"].mean()
+print("Average temp:", average_temp)
