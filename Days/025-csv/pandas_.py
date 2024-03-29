50f16e8a70787c5163cf33de95f8cd6c745ab418
Christian Eriksson <kryckan@gmail.com>
2024-03-29 01:28:21 +0100

    🔄 Refactor and optimize CSV data handling.

---
+++ Days/025-csv/pandas_.py
@@ -0,0 +1,22 @@
+import os
+
+import pandas as pd
+
+root_directory = os.path.dirname(__file__)
+file_path = os.path.join(root_directory, "weather_data.csv")
+
+data = pd.read_csv(file_path)
+
+
+# average_temp = data["temp"].mean()
+# print("Average temp:", data["temp"].mean())
+# print("Max temp: ", data["temp"].max())
+# print("Min temp: ", data["temp"].min())
+
+# print(data[data.temp == data.temp.max()])
+
+monday_data = data[data.day == "Monday"]
+
+print(monday_data.temp)
+monday_temp_f = (monday_data.temp * 9 / 5) + 32
+print("Monday's temperature in Fahrenheit:", monday_temp_f)
---
+++ Days/025-us-states-game-start/main.py
@@ -0,0 +1,64 @@
+import turtle
+
+import pandas as pd
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "Days/025-us-states-game-start/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+exit = False
+
+correct_guesses: set[str] = set()
+score_display = turtle.Turtle()
+score_display.hideturtle()
+score_display.penup()
+score_display.goto(0, 270)
+score_display.write("Score: 0", align="center", font=("Courier", 24, "normal"))
+
+answers: pd.DataFrame = pd.read_csv("Days/025-us-states-game-start/50_states.csv")
+
+
+def update_score_display():
+    score_display.clear()
+    score_display.write(
+        f"Score: {len(correct_guesses)}", align="center", font=("Courier", 24, "normal")
+    )
+
+
+def new_turtle(x: int, y: int, state: str):
+    state_turtle = turtle.Turtle()
+    state_turtle.hideturtle()
+    state_turtle.penup()
+    state_turtle.goto(int(x), int(y))
+    state_turtle.write(state)
+
+
+def get_answer() -> bool:
+    global exit
+    answer_state = screen.textinput(
+        title="Guess the State", prompt="What's another state's name?"
+    )
+    answer_titled: str = answer_state.title() if answer_state else ""
+    if (
+        answer_titled in answers["state"].values
+        and answer_titled not in correct_guesses
+    ):
+        correct_guesses.add(answer_titled)
+        update_score_display()
+        return True
+    if answer_titled == "Exit":
+        exit = True
+    return False
+
+
+while True:
+    if exit:
+        break
+    if get_answer():
+        state_name = list(correct_guesses)[-1]
+        state_data = answers[answers.state == state_name]
+        x_cor, y_cor = state_data.x.iloc[0], state_data.y.iloc[0]
+        new_turtle(x_cor, y_cor, state_name)
+
+turtle.mainloop()
