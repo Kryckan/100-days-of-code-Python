5410a4551394c430dc8aec2831325384e311a240
Christian Eriksson <kryckan@gmail.com>
2024-04-07 23:50:17 +0200

    mplement font selection based on installed fonts, use OS path for files and improve UI layout

--- Days/028-pomodoro/main.py
+++ Days/028-pomodoro/main.py
@@ -1,7 +1,7 @@
-import time
+import os
 
 from PyQt5.QtCore import Qt, QTimer
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QFontDatabase, QPixmap
 from PyQt5.QtWidgets import (
     QApplication,
     QHBoxLayout,
@@ -16,7 +16,8 @@ PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
-FONT_NAME = "Courier"
+FONT_NAMES = ["Poppins", "Courier", "Arial", "Helvetica", "Times New Roman"]
+FONT_NAME = ""
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
@@ -26,27 +27,50 @@ countdown_on = False
 timer_seconds = 0
 timer_label = None
 countdown_timer = None
+start_button_text: str = "Start"
+start_button = None
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+big_tomato = os.path.join(current_dir, "tomato.png")
+
+
+def check_fonts_availability():
+    global FONT_NAME
+    font_db = QFontDatabase()
+    available_fonts = font_db.families()
+
+    for font_name in FONT_NAMES:
+        if font_name in available_fonts:
+            FONT_NAME = font_name
+            break
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
 def reset_timer():
-    global countdown_on, timer_seconds
+    global countdown_on, timer_seconds, start_button
     countdown_on = False
     timer_seconds = 0
     if timer_label is not None:  # Ensure timer_label is initialized
         timer_label.setText("25:00")
     if countdown_timer is not None:  # Ensure countdown_timer is initialized
         countdown_timer.stop()
+    if start_button is not None:
+        start_button.setText("Start")
 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    global countdown_on, timer_seconds, countdown_timer
-    countdown_on = True
-    timer_seconds = WORK_MIN * 60 - 1
-    countdown_timer = QTimer()
-    countdown_timer.timeout.connect(update_timer)
-    countdown_timer.start(1000)
+    global countdown_on, timer_seconds, countdown_timer, start_button
+    if countdown_on:
+        reset_timer()
+    else:
+        countdown_on = True
+        timer_seconds = WORK_MIN * 60 - 1
+        if start_button is not None:
+            start_button.setText("Stop")
+        countdown_timer = QTimer()
+        countdown_timer.timeout.connect(update_timer)
+        countdown_timer.start(1000)
 
 
 def update_timer():
@@ -64,14 +88,15 @@ def update_timer():
 
 # ---------------------------- UI SETUP ------------------------------- #
 def setup_ui(window):
-    global timer_label
+    global timer_label, start_button, big_tomato
+    print(current_dir)
     window.setWindowTitle("Pomodoro")
     window.setFixedSize(300, 330)
     window.setObjectName("MainWindow")
     window.setStyleSheet(
         f"""
         #MainWindow {{
-            background-image: url('Days/028-pomodoro/tomato.png');
+            background-image: url('{big_tomato}');
             background-repeat: no-repeat;
             background-position: center;
             background-color: {YELLOW};
@@ -109,7 +134,7 @@ def setup_ui(window):
 
     buttons_layout = QHBoxLayout()
 
-    start_button = QPushButton("Start")
+    start_button = QPushButton(start_button_text)
     start_button.setStyleSheet(
         """
         QPushButton {
