bed743caa73b8f139684c45a079cc3ca51e559e8
Christian Eriksson <kryckan@gmail.com>
2024-12-23 18:42:01 +0100

    feat: Implement ISS visibility check based on sun position

---
+++ Days/033-API/iss_pos.py
@@ -0,0 +1,60 @@
+import requests
+
+
+class ISSPosition:
+    def __init__(self):
+        self.api_url = "http://api.open-notify.org/iss-now.json"
+
+    def get_iss_position(self):
+        response = requests.get(url=self.api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+import requests
+
+
+class ISSPosition:
+    def __init__(self):
+        self.api_url = "http://api.open-notify.org/iss-now.json"
+
+    def get_iss_position(self):
+        response = requests.get(url=self.api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+import requests
+
+
+class ISSPosition:
+    def __init__(self):
+        self.api_url = "http://api.open-notify.org/iss-now.json"
+
+    def get_iss_position(self):
+        response = requests.get(url=self.api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+import requests
+
+
+class ISSPosition:
+    def __init__(self):
+        self.api_url = "http://api.open-notify.org/iss-now.json"
+
+    def get_iss_position(self):
+        response = requests.get(url=self.api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
+import requests
+
+
+class ISSPosition:
+    def __init__(self):
+        self.api_url = "http://api.open-notify.org/iss-now.json"
+
+    def get_iss_position(self):
+        response = requests.get(url=self.api_url)
+        response.raise_for_status()
+        data = response.json()
+        return data
