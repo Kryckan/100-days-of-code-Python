399b44cfe391db9d52b6a89a8eade7077eb58410
Christian Eriksson <kryckan@gmail.com>
2024-12-23 18:44:01 +0100

    ce-branch-1 (#16)
    * feat: Implement error handling in main.py
    
    * feat: Implement email sending functionality
    
    * feat: Implement ISS visibility check based on sun position

---
+++ Days/030-errors/main.py
@@ -0,0 +1,72 @@
+file = None
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise Exception("This is an error message")
+    if file:
+        file.close()
+    print("File was closed.")
+file = None
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise Exception("This is an error message")
+    if file:
+        file.close()
+    print("File was closed.")
+file = None
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise Exception("This is an error message")
+    if file:
+        file.close()
+    print("File was closed.")
+file = None
+try:
+    file = open("a_file.txt")
+    a_dictionary = {"key": "value"}
+    print(a_dictionary["key"])
+except FileNotFoundError:
+    file = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist.")
+else:
+    content = file.read()
+    print(content)
+finally:
+    raise Exception("This is an error message")
+    if file:
+        file.close()
+    print("File was closed.")
---
+++ Days/032-Emails/main.py
@@ -0,0 +1,65 @@
+import smtplib
+
+my_email = "@gmail.com"
+password = "1234567890"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\nThis is the body of my email",
+    )
+import smtplib
+
+my_email = "@gmail.com"
+password = "1234567890"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\nThis is the body of my email",
+    )
+import smtplib
+
+my_email = "@gmail.com"
+password = "1234567890"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\nThis is the body of my email",
+    )
+import smtplib
+
+my_email = "@gmail.com"
+password = "1234567890"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\nThis is the body of my email",
+    )
+import smtplib
+
+my_email = "@gmail.com"
+password = "1234567890"
+
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="",
+        msg="Subject:Hello\n\nThis is the body of my email",
+    )
