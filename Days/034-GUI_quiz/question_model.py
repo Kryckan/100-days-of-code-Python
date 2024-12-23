db601668c69c6dc376cb336951226b6563ef1dfc
Christian Eriksson <kryckan@gmail.com>
2024-12-23 18:42:48 +0100

    feat: Implement GUI quiz application

---
+++ Days/034-GUI_quiz/question_model.py
@@ -0,0 +1,25 @@
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
+class Question:
+
+    def __init__(self, q_text, q_answer):
+        self.text = q_text
+        self.answer = q_answer
