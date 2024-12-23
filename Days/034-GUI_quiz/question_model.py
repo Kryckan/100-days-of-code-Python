d28e06b1c591f2443090b7d7fa51fbb8f2f792b6
Christian Eriksson <kryckan@gmail.com>
2024-12-23 18:54:38 +0100

    Clean up weird copies in code. (#17)

--- Days/034-GUI_quiz/question_model.py
+++ Days/034-GUI_quiz/question_model.py
@@ -3,23 +3,3 @@ class Question:
     def __init__(self, q_text, q_answer):
         self.text = q_text
         self.answer = q_answer
-class Question:
-
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
-class Question:
-
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
-class Question:
-
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
-class Question:
-
-    def __init__(self, q_text, q_answer):
-        self.text = q_text
-        self.answer = q_answer
